65df00f0bb89c15dc772a26bc761105eeee8f987
oamoam <71397516+oamoam@users.noreply.github.com>
2020-11-23 18:48:16 +0100

    Add files via upload

---
+++ Attendance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Attendance
+    {
+        public List<DateTime> absenceList;
+        public List<DateTime> CourseTeached
+        {
+            get
+            {
+                return this.absenceList;
+            }
+            set
+            {
+                this.absenceList = value;
+            }
+        }
+        public void DisplayOwnAttendance()
+        {
+            Console.WriteLine("You have " + absenceList.Count + " absences");
+            if (absenceList.Count != 0) 
+            {
+                for (int i = 0; i < absenceList.Count; i++)
+                {
+                    Console.WriteLine("You were absent the " + absenceList[i].Date);
+                }
+
+            }
+
+        }
+    }
+}
